171d609488558e34db6fe86deeb4da33f8a5c655
RLASH18 <lacdangryan18@gmail.com>
2025-07-22 21:33:54 +0800

    Initial Commit

--- app/views/home.view.php
+++
@@ -1,8 +0,0 @@
-<!-- loads the header in the views/layouts -->
-<?php layout('header') ?>
-
-<h1>hello <?= auth()->email ?></h1>
-
-
-<!-- loads the footer in the views/layouts -->
-<?php layout('footer') ?>
