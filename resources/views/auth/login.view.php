23f37f9b2e311c082bb13fdc9da8b6602f689e72
reylanceg <reylanceg@gmail.com>
2025-08-09 14:08:19 +0800

    Created login page

--- resources/views/auth/login.view.php
+++ resources/views/auth/login.view.php
@@ -1,34 +1,55 @@
 <?php layout('auth/header') ?>
 
-    <form action="/loginForm" method="post">
-
-        <?= csrf_token() ?>
-
-        <?= flash('success') ?>
-
-        <?= flash('error') ?>
-
-
-        <div>
-            <label for="login">login</label>
-            <input type="text" name="login" id="login" value="<?= old('login') ?>" <?= isInvalid('login') ?>>
-            <div class="text-red-500">
-                <p><?= error('login') ?></p>
+    <div class="login-page-container">
+        <div class="login-container">
+            <div class="header-container">
+                <div class="sign-in-container">
+                    <h1>Sign In</h1>
+                </div>
+                <p>Welcome back to ABG Prime Builders Supplies Inc.</p>
             </div>
-        </div>
+            <form action="/loginForm" method="post">
 
-        <div>
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password" <?= isInvalid('password') ?>>
-            <div class="text-red-500">
-                <p><?= error('password') ?></p>
-            </div>
-        </div>
+                <?= csrf_token() ?>
 
-        <div>
-            <button type="submit">submit</button>
-        </div>
+                <?= flash('success') ?>
 
-    </form>
+                <?= flash('error') ?>
+
+
+                <div class="input-container">
+                    <div>
+                        <input type="text" name="login" id="login" placeholder="username/email" value="<?= old('login') ?>" <?= isInvalid('login') ?>>
+                        <div class="text-red-500">
+                            <p><?= error('login') ?></p>
+                        </div>
+                    </div>
+
+                    <div>
+                        <input type="password" name="password" id="password" placeholder="password" <?= isInvalid('password') ?>>
+                        <div class="text-red-500">
+                            <p><?= error('password') ?></p>
+                        </div>
+                    </div>
+                    <div class="remember">
+                        <label><input type="checkbox" />Remember me</label>
+                    </div>
+                </div>
+
+                <div class="buttons">
+                    <button class="sign-in">Sign In</button>
+                    <h4><span>OR</span></h4>
+                    <button class="sign-up">Sign Up</button>
+                    <div class="have-account">
+                        <p>
+                        Don't have an account? Create your account, it takes less than a
+                        minute.
+                        </p>
+                    </div>
+                </div>
+
+            </form>
+        </div>
+    </div>
 
 <?php layout('auth/footer') ?>
